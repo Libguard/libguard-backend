165555ec7c673b94ddc72a5803b69a9b183d6964
harisoncleytondev <harisonc081@gmail.com>
2026-02-18 17:16:39 -0300

    feat(authentication): implementa login e adiciona rota de autenticação com ajuste na response do usuário

--- apps/authentication/services.py
+++ apps/authentication/services.py
@@ -1,10 +1,12 @@
 from apps.accounts.models import User, UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.exceptions import AuthenticationFailed
 from django.utils import timezone
 from datetime import timedelta
 from apps.authentication.models import RefreshToken as RefreshModel
 from django.db import transaction
 from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate
 import hashlib
 
 def _gen_tokens(user:  User, ip):
@@ -41,3 +43,23 @@ def create_user_service(email, name, password, ip): # vai chamar esse metodo, q 
         }
     except Exception as e:
         raise ValidationError(f"Falha na criação do usuário: {str(e)}")
+
+@transaction.atomic
+def login_user_service(email, password, ip):
+    try:
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise AuthenticationFailed("E-mail ou senha inválidos") 
+        
+        refresh, access = _gen_tokens(user= user, ip= ip)
+
+        return {
+            "refresh": refresh,
+            "access": access,
+            "user": UserSerializer(user).data
+        }
+    except AuthenticationFailed as e:
+        raise e
+    except Exception as e:
+        raise ValidationError(f"Falha ao validar o usuário: {str(e)}")
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework import status
 from django.core.exceptions import ValidationError
 from apps.authentication.serializers import RegisterSerializer, LoginSerializer, RefreshTokenSerializer
-from apps.authentication.services import create_user_service
+from apps.authentication.services import create_user_service, login_user_service
 
 class RegisterView(APIView):
     def post(self, request): # rota pra registrar, vai chamar o service
@@ -23,11 +24,31 @@ class RegisterView(APIView):
             return Response({
                 "refresh": result["refresh"],
                 "access": result["access"],
-                "user": {
-                    "user_id": result["user"],
-                    "email": result["user"],
-                    "name": result["user"]
-                }
+                "user": result["user"]
             }, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class LoginView(APIView):
+    def post(self, request):
+        try:
+            serializer = LoginSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+
+            ip = request.META.get('REMOTE_ADDR')
+
+            result = login_user_service(
+                email= serializer.validated_data['email'],
+                password= serializer.validated_data['password'],
+                ip=ip
+            )
+
+            return Response({
+                "refresh": result["refresh"],
+                "access": result["access"],
+                "user": result["user"]
+            }, status=status.HTTP_200_OK)
+        except AuthenticationFailed as e:
+            return Response({"Não autorizado.": str(e)}, status=status.HTTP_401_UNAUTHORIZED)
         except ValidationError as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
