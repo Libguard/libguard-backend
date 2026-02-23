d99e6c26bbc83750625ddfe179f22c20c76eea1f
harisoncleytondev <harisonc081@gmail.com>
2026-02-22 21:11:26 -0300

    fix(settings): adiciona import do timedelta no settings.py

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,8 +1,10 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 from apps.projects.serializers.project_serializer import ProjectSerializer
 
 class ProjectCreateView(CreateAPIView):
     serializer_class = ProjectSerializer
+    permission_class = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
